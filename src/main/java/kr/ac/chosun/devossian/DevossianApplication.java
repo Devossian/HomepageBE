a42169f667aebf9fff7bb16703d575f9789247c8
scorve12 <scorve5322@gmail.com>
2025-02-12 06:36:12 +0900

    init: 프로젝트 초기화

---
+++ src/main/java/kr/ac/chosun/devossian/DevossianApplication.java
@@ -0,0 +1,12 @@
+package kr.ac.chosun.devossian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevossianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevossianApplication.class, args);
+	}
+}
---
+++ src/test/java/kr/ac/chosun/devossian/DevossianApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.chosun.devossian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevossianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
